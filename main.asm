31e7b03dcd04b072a77046c2df65a47f6b449488
Hadif Hatta <45189724+hdfhtt@users.noreply.github.com>
2023-06-12 12:06:59 +0800

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,84 +1,101 @@
 .data
-	app_title: .asciiz "SCHOOL RUN APP\n"
-	vehicle_plate: .space 4
+	temp_concat: .space 64
+	temp_plate_no: .space 8
 	
-	text_arriving: .asciiz "Vehicle with plate ("
-	text_arriving_e: .asciiz ") is arriving.\n"
+	str_arriving1: .asciiz "A vehicle with plate no. of "
+	str_arriving2: .asciiz " is heading to the PICKUP point..."
+	str_arriving3: .asciiz " is heading to the DROPOFF point..."
+
+	str_plate_sensor: .asciiz "Plate Sensor Input:"
+	str_restart: .asciiz "Scanning for a new vehicle..."
 	
-	dialog_route: .asciiz "Is vehicle heading to the DROPOFF lane?"
+	str_route_select: .asciiz "Is vehicle heading to DROPOFF lane?"
 	
-.text 
+.text
 main:
-	AppStart:
-	# Generate a 3-digit random plate number
-	li $v0, 42
-	li $a0, 100
-	li $a1, 999
-	syscall
+	j app_start
 	
-	# Store generated plate number into data segment
-	sw $a0, vehicle_plate
+	app_restart:
+		li $v0, 55			# Call MessageDialog function
+		la $a0, str_restart		# Load str_restart as the message
+		syscall
 	
-	li $v0, 4
-	la $a0, app_title
-	syscall
-	
-	# Vehicle Recognization:
-	li $v0, 4
-	la $a0, text_arriving
-	syscall
-	
-	li $v0, 1
-	lw $a0, vehicle_plate
-	syscall
-	
-	li $v0, 4
-	la $a0, text_arriving_e
-	syscall
-	
-	# Route Detection:
-	li $v0, 50
-	la $a0, dialog_route
-	syscall
-	
-	beq $a0, 2, AppStart
-	
-	AppExit:
+	app_start:
+		# Sensor1: Plate No Sensor
+		li $v0, 54			# Call InputDialogString function
+		la $a0, str_plate_sensor
+		la $a1, temp_plate_no		# Address of the input
+		li $a2, 8			# Maximum number of characters to read
+		syscall
+		
+		bnez $a1, app_restart
+		
+		# Sensor2: Route Detection
+		li $v0, 50			# Call ConfirmDialog function
+		la $a0, str_route_select	# Load str_route_select as the message
+		syscall
+		
+		# Actuator1: Route Detection Display
+		move $s1, $a0			# Move result of syscall 50 in $a0 to $s1
+		la $a0, str_arriving1		# Load str_arriving1
+		la $a1, temp_plate_no		# Load plate number
+		beq $s1, 2, app_restart		# If CANCEL is selected, jump to app_restart
+		beqz $s1, point_dropoff		# If YES is selected, jump to point_dropoff
+		
+		point_pickup: 
+			la $a2, str_arriving2	# Load str_arriving2 (pickup)
+			j point_actuator
+			
+		point_dropoff: 
+			la $a2, str_arriving3 	# Load str_arriving3 (dropoff)
+			
+		point_actuator:
+			la $a3, temp_concat
+			jal concat_strings	# Call the concat_strings function
+			
+		li $v0, 55			# Call MessageDialog function
+		la $a0, temp_concat		# Load saved concatenated strings as the message
+		syscall
+
+	# Terminate the program
 	li $v0, 10
 	syscall
 
-# Helper Functions
+# Helper functions
+# concat_strings(str1, str2, str3, return)
 concat_strings:
-	# Initialize registers
-	li $t0, 0			# Counter for first string
-	li $t1, 0			# Counter for second string
-	
-	# Search the end of first string
-	loop_1:
-		lb $t2, ($a0)		# Load a byte from the first string
-		beqz $t2, loop_2	# If the byte is zero (end of the string)
-		addiu $a0, $a0, 1	# Increment the first string pointer
-		addiu $t0, $t0, 1	# Increment the counter
-		j loop_1
-	
-	# Search the end of second string
-	loop_2:
-		lb $t3, ($a1)		# Load a byte from the second string
-		beqz $t3, loop_3
-		addiu $a0, $a0, 1	# Increment the first string pointer
-		addiu $a1, $a1, 1	# Increment the second string pointer
-		addiu $t1, $t1, 1	# Increment the counter
-		j loop_2
-	
-	# Copy the third string to the end of the concatenated string
-	loop_3:
-		lb $t4, ($a2)		# Load a byte from the third string
-		beqz $t4, result
-		sb $t4, ($a0)		# Store the byte at the end of the concatenated string
-		addiu $a0, $a0, 1	# Increment the concatenated string pointer
-		addiu $a2, $a2, 1	# Increment the third string pointer
-		j loop_3
-	
-	# The result of concatenated string is in the memory address of the first string
-	result:
+	move $t0, $a0				# Save the address of str1
+	move $t1, $a3				# Save the address of result
+
+	# Copy str1 to result
+	cs_loop1:
+		lb $t2, ($t0)			# Load a character from str1
+		sb $t2, ($t1)			# Store the character in result
+
+		beqz $t2, cs_loop2		# If the character is null, proceed to loop2
+		addiu $t0, $t0, 1		# Increment str1 pointer
+		addiu $t1, $t1, 1		# Increment result pointer
+		j cs_loop1			# Repeat for the next character
+
+	# Copy str2 to result
+	cs_loop2:
+		lb $t2, ($a1)			# Load a character from str2
+		sb $t2, ($t1)			# Store the character in result
+
+		beqz $t2, cs_loop3		# If the character is null, proceed to loop3
+		addiu $a1, $a1, 1		# Increment str2 pointer
+		addiu $t1, $t1, 1		# Increment result pointer
+		j cs_loop2			# Repeat for the next character
+
+	# Copy str3 to result
+	cs_loop3:
+		lb $t2, ($a2)			# Load a character from str3
+		sb $t2, ($t1)			# Store the character in result
+
+		beqz $t2, cs_end		# If the character is null, we're done
+		addiu $a2, $a2, 1		# Increment str3 pointer
+		addiu $t1, $t1, 1		# Increment result pointer
+		j cs_loop3			# Repeat for the next character
+
+	cs_end:
 		jr $ra
