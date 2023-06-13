8839638ebb90d507d26aa4a92ac94f0c4eb82f28
Hadif Hatta <45189724+hdfhtt@users.noreply.github.com>
2023-06-13 22:45:44 +0800

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,101 +1,105 @@
 .data
-	temp_concat: .space 64
-	temp_plate_no: .space 8
+	dlg_plate_no: .asciiz "Plate Sensor Input: (max 8 characters)"
+	dlg_route_detect: .asciiz "Route Detection:\n[ YES ] Dropoff Lane\n[ NO ] Pickup Lane"
+	dlg_dropoff: .asciiz "A vehicle has dropped off the student(s). Student attendance has been updated."
 	
-	str_arriving1: .asciiz "A vehicle with plate no. of "
-	str_arriving2: .asciiz " is heading to the PICKUP point..."
-	str_arriving3: .asciiz " is heading to the DROPOFF point..."
+	dlg_restart: .asciiz "Scanning for a new vehicle..."
+	dlg_debug_continue: .asciiz "(Debug) Do you wish to continue?"
+	
+	dlg_pickup: .asciiz "A vehicle has arrived in the pickup lane. Is the student(s) for that vehicle ready?"
+	dlg_pickup_wait: .asciiz "Direct the vehicle to the waiting lane. Waiting for the student(s) to be ready..."
+	
+	sys_arriving1: .asciiz "Log: A vehicle with plate no. of "
+	sys_arriving2: .asciiz " is arriving...\n"
+	
+	plate_no: .space 9
 
-	str_plate_sensor: .asciiz "Plate Sensor Input:"
-	str_restart: .asciiz "Scanning for a new vehicle..."
-	
-	str_route_select: .asciiz "Is vehicle heading to DROPOFF lane?"
-	
 .text
 main:
-	j app_start
+	j app_start				# Start the app with app_start
 	
 	app_restart:
 		li $v0, 55			# Call MessageDialog function
-		la $a0, str_restart		# Load str_restart as the message
-		syscall
+		la $a0, dlg_restart		# Load sys_restart as the message
+		li $a1, 2			# Set type of the message to 2 (warning)
+		syscall				# Call syscall to initiate the function
 	
 	app_start:
 		# Sensor1: Plate No Sensor
 		li $v0, 54			# Call InputDialogString function
-		la $a0, str_plate_sensor
-		la $a1, temp_plate_no		# Address of the input
-		li $a2, 8			# Maximum number of characters to read
+		la $a0, dlg_plate_no		# Load dlg_sens_plate as the message
+		la $a1, plate_no		# Address of the input buffer
+		li $a2, 9			# Maximum number of characters to read
 		syscall
 		
+		# Actuator1: Plate No Validator
+		# If the result is either CANCEL or INVALID data, assume there is no vehicle detected
+		# And start scanning for a new vehicle (app_restart)
 		bnez $a1, app_restart
 		
-		# Sensor2: Route Detection
+		# Display the first part of the message
+		li $v0, 4
+		la $a0, sys_arriving1
+		syscall
+		
+		# Display plate no as the second part of the message
+		li $v0, 4
+		la $a0, plate_no
+		syscall
+		
+		# Display the third part of the message
+		li $v0, 4
+		la $a0, sys_arriving2
+		syscall
+		
+		# Sensor2: Vehicle Routing Sensor
 		li $v0, 50			# Call ConfirmDialog function
-		la $a0, str_route_select	# Load str_route_select as the message
+		la $a0, dlg_route_detect	# Load dlg_route_select as the message
 		syscall
 		
-		# Actuator1: Route Detection Display
-		move $s1, $a0			# Move result of syscall 50 in $a0 to $s1
-		la $a0, str_arriving1		# Load str_arriving1
-		la $a1, temp_plate_no		# Load plate number
-		beq $s1, 2, app_restart		# If CANCEL is selected, jump to app_restart
-		beqz $s1, point_dropoff		# If YES is selected, jump to point_dropoff
+		# Actuator2: Vehicle Routing Actuator
+		# If CANCEL is selected, assume the vehicle has no longer in the route
+		# and start scanning for a new vehicle (app_restart)
+		beq $a0, 2, app_restart
+		# If YES is selected, jump to point_dropoff
+		# otherwise, continue to point_pickup
+		beqz $a0, point_dropoff
 		
-		point_pickup: 
-			la $a2, str_arriving2	# Load str_arriving2 (pickup)
-			j point_actuator
+		point_pickup:
+			# Sensor3: Student Checker
+			li $v0, 50		# Call ConfirmDialog function
+			la $a0, dlg_pickup	# Load dlg_pickup as the message
+			syscall
 			
-		point_dropoff: 
-			la $a2, str_arriving3 	# Load str_arriving3 (dropoff)
+			# Actuator3: Vehicle Lane Selector
+			# If CANCEL is selected, assume the vehicle has no longer in the route
+			beq $a0, 2, app_restart
+			# If NO is selected, send vehicle to waiting lane
+			bnez $a0, point_pickup_wait
+		
+		point_pickup_wait:
+			li $v0, 55		# Call MessageDialog function
+			la $a0, dlg_pickup_wait	# Load dlg_pickup_wait as the message
+			li $a1, 1		# Set the type of message to 1 (information)
+			syscall
 			
-		point_actuator:
-			la $a3, temp_concat
-			jal concat_strings	# Call the concat_strings function
+			j point_pickup		# Jump to point_pickup until the student(s) is ready
+		
+		point_dropoff:
+			li $v0, 55		# Call MessageDialog function
+			la $a0, dlg_dropoff	# Load dlg_dropoff as the message
+			li $a1, 1		# Set the type of message to 1 (information)
+			syscall
 			
-		li $v0, 55			# Call MessageDialog function
-		la $a0, temp_concat		# Load saved concatenated strings as the message
+	app_debug_continue:
+		li $v0, 50			# Call ConfirmDialog function
+		la $a0, dlg_debug_continue	# Load dlg_debug_continue as the message
 		syscall
-
-	# Terminate the program
-	li $v0, 10
-	syscall
-
-# Helper functions
-# concat_strings(str1, str2, str3, return)
-concat_strings:
-	move $t0, $a0				# Save the address of str1
-	move $t1, $a3				# Save the address of result
-
-	# Copy str1 to result
-	cs_loop1:
-		lb $t2, ($t0)			# Load a character from str1
-		sb $t2, ($t1)			# Store the character in result
-
-		beqz $t2, cs_loop2		# If the character is null, proceed to loop2
-		addiu $t0, $t0, 1		# Increment str1 pointer
-		addiu $t1, $t1, 1		# Increment result pointer
-		j cs_loop1			# Repeat for the next character
-
-	# Copy str2 to result
-	cs_loop2:
-		lb $t2, ($a1)			# Load a character from str2
-		sb $t2, ($t1)			# Store the character in result
-
-		beqz $t2, cs_loop3		# If the character is null, proceed to loop3
-		addiu $a1, $a1, 1		# Increment str2 pointer
-		addiu $t1, $t1, 1		# Increment result pointer
-		j cs_loop2			# Repeat for the next character
-
-	# Copy str3 to result
-	cs_loop3:
-		lb $t2, ($a2)			# Load a character from str3
-		sb $t2, ($t1)			# Store the character in result
-
-		beqz $t2, cs_end		# If the character is null, we're done
-		addiu $a2, $a2, 1		# Increment str3 pointer
-		addiu $t1, $t1, 1		# Increment result pointer
-		j cs_loop3			# Repeat for the next character
-
-	cs_end:
-		jr $ra
+		
+		# If YES is selected, jump to app_restart
+		# Else proceed to app_exit
+		beqz $a0, app_restart
+		
+	app_exit:
+		li $v0, 10			# Call exit function
+		syscall
